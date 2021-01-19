a6341ed6e41a1515ae2e2b967aed4edc4876d459
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-19 22:03:32 +0200

    Added database with JSON file

--- cats.js
+++ cats.js
@@ -1,12 +1,20 @@
-const cats = [];
+const fs = require('fs')
+const catsData = require('./cats.json');
+const cats = catsData.slice();
 
 function add(name) {
     cats.push(name);
-
+    fs.writeFile('./cats.json', JSON.stringify(cats), (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Successful write');
+    });
 }
 
-function getAll(){
-    return cats.slice(); 
+function getAll() {
+    return cats.slice();
 }
 
 
