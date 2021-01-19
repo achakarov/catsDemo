ab1a4cfd7f031f5a62e013d0d302f42f69593ab1
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-19 18:43:05 +0200

    Initiate server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world from Express!');
+});
+
+app.listen(port, () => console.log(`This server is running on port ${port}`)); 
