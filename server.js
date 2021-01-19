b65636aa8303c2ea823ee3d229ca155aa9310185
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-01-19 21:57:02 +0200

    Added template for showing Cats

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 // const checkCatIdMiddleware = require('./middlewares/middleware');
 // const loggerMiddleware = require('./middlewares/logger');
 
 const app = express();
 const port = 5000;
-const cats = [];
+const cats = require('./cats');
 
 // app.use(express.static('public'));
 // app.use(loggerMiddleware);
-app.engine('hbs', handlebars());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     let name = 'Navcho';
-   res.render('home', {layout: false, name});
+    res.render('home', { layout: false, name });
 });
 
 app.get('/download', (req, res) => {
     res.download('./views/home.html');
 });
 
-// app.get('/cats', (req, res) => {
-//     res.redirect('/');
-// });
-
-app.get('/cats/:catId?', (req, res) => {
-    if (!/\d+/.test(req.params.catId)) {
-        res.status(404).send('You need to specify cat ID number');
-        return;
-    }
-    res.send(`You are looking at profile of ${req.params.catId}`);
+app.get('/cats', (req, res) => {
+    res.render('cats', {cats: cats.getAll()});
 });
 
-app.post('/cats', (req, res) => {
-    console.log('Create cat');
+// app.get('/cats/:catId?', (req, res) => {
+//     if (!/\d+/.test(req.params.catId)) {
+//         res.status(404).send('You need to specify cat ID number');
+//         return;
+//     }
+//     res.send(`You are looking at profile of ${req.params.catId}`);
+// });
 
-    res
-        .status(201)
-        .send('Cat created!');
+app.post('/cats', (req, res) => {
+    let catName = req.body.cat;
+    cats.add(catName)
+    res.redirect('/cats');
 });
 
 // app.put('/cats/:id', (req, res)=>{
